0f5c4a92086e61dff12006a6c1ccf254ef4c8b30
Devansh Baghel <devanshbaghel85@gmail.com>
2023-12-16 20:53:41 +0530

    add daisyui, style Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='p-6'>
+      <div className='flex flex-col gap-4 items-center'>
+        <h1 className='text-center text-3xl font-bold'>Message Board</h1>
+        <a href="https://github.com/Devansh-Baghel/message-board" className=''>
+          <button className='btn btn-neutral'>🔗 Source Code</button>
+        </a>
+      </div>
+    </div>
   )
 }
 
